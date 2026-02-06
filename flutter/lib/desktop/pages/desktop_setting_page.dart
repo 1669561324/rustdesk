7fecfdf27b2d99fcac55e5cd425f5828b8c33e29
okma <3039699214@qq.com>
2026-02-06 17:42:09 +0800

    fix bug

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1066,161 +1066,162 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
   }
 
   Widget password(BuildContext context) {
-    return ChangeNotifierProvider.value(
-        value: gFFI.serverModel,
-        child: Consumer<ServerModel>(builder: ((context, model, child) {
-          List<String> passwordKeys = [
-            kUseTemporaryPassword,
-            kUsePermanentPassword,
-            kUseBothPasswords,
-          ];
-          List<String> passwordValues = [
-            translate('Use one-time password'),
-            translate('Use permanent password'),
-            translate('Use both passwords'),
-          ];
-          bool tmpEnabled = model.verificationMethod != kUsePermanentPassword;
-          bool permEnabled = model.verificationMethod != kUseTemporaryPassword;
-          String currentValue =
-              passwordValues[passwordKeys.indexOf(model.verificationMethod)];
-          List<Widget> radios = passwordValues
-              .map((value) => _Radio<String>(
-                    context,
-                    enabled：false,
-                    value: value,
-                    groupValue: currentValue,
-                    label: value,
-                    onChanged: locked
-                        ? null
-                        : ((value) async {
-                            callback() async {
-                              await model.setVerificationMethod(
-                                  passwordKeys[passwordValues.indexOf(value)]);
-                              await model.updatePasswordModel();
-                            }
+    return Container();
+    // return ChangeNotifierProvider.value(
+        // value: gFFI.serverModel,
+        // child: Consumer<ServerModel>(builder: ((context, model, child) {
+        //   List<String> passwordKeys = [
+        //     kUseTemporaryPassword,
+        //     kUsePermanentPassword,
+        //     kUseBothPasswords,
+        //   ];
+        //   List<String> passwordValues = [
+        //     translate('Use one-time password'),
+        //     translate('Use permanent password'),
+        //     translate('Use both passwords'),
+        //   ];
+        //   bool tmpEnabled = model.verificationMethod != kUsePermanentPassword;
+        //   bool permEnabled = model.verificationMethod != kUseTemporaryPassword;
+        //   String currentValue =
+        //       passwordValues[passwordKeys.indexOf(model.verificationMethod)];
+        //   List<Widget> radios = passwordValues
+        //       .map((value) => _Radio<String>(
+        //             context,
+        //             enabled：false,
+        //             value: value,
+        //             groupValue: currentValue,
+        //             label: value,
+        //             onChanged: locked
+        //                 ? null
+        //                 : ((value) async {
+        //                     callback() async {
+        //                       await model.setVerificationMethod(
+        //                           passwordKeys[passwordValues.indexOf(value)]);
+        //                       await model.updatePasswordModel();
+        //                     }
 
-                            if (value ==
-                                    passwordValues[passwordKeys
-                                        .indexOf(kUsePermanentPassword)] &&
-                                (await bind.mainGetPermanentPassword())
-                                    .isEmpty) {
-                              if (isChangePermanentPasswordDisabled()) {
-                                await callback();
-                                return;
-                              }
-                              setPasswordDialog(notEmptyCallback: callback);
-                            } else {
-                              await callback();
-                            }
-                          }),
-                  ))
-              .toList();
+        //                     if (value ==
+        //                             passwordValues[passwordKeys
+        //                                 .indexOf(kUsePermanentPassword)] &&
+        //                         (await bind.mainGetPermanentPassword())
+        //                             .isEmpty) {
+        //                       if (isChangePermanentPasswordDisabled()) {
+        //                         await callback();
+        //                         return;
+        //                       }
+        //                       setPasswordDialog(notEmptyCallback: callback);
+        //                     } else {
+        //                       await callback();
+        //                     }
+        //                   }),
+        //           ))
+        //       .toList();
 
-          var onChanged = tmpEnabled && !locked
-              ? (value) {
-                  if (value != null) {
-                    () async {
-                      await model.setTemporaryPasswordLength(value.toString());
-                      await model.updatePasswordModel();
-                    }();
-                  }
-                }
-              : null;
-          List<Widget> lengthRadios = ['6', '8', '10']
-              .map((value) => GestureDetector(
-                    child: Row(
-                      children: [
-                        Radio(
-                            value: value,
-                            groupValue: model.temporaryPasswordLength,
-                            onChanged: onChanged),
-                        Text(
-                          value,
-                          style: TextStyle(
-                              color: disabledTextColor(
-                                  context, onChanged != null)),
-                        ),
-                      ],
-                    ).paddingOnly(right: 10),
-                    onTap: () => onChanged?.call(value),
-                  ))
-              .toList();
+        //   var onChanged = tmpEnabled && !locked
+        //       ? (value) {
+        //           if (value != null) {
+        //             () async {
+        //               await model.setTemporaryPasswordLength(value.toString());
+        //               await model.updatePasswordModel();
+        //             }();
+        //           }
+        //         }
+        //       : null;
+        //   List<Widget> lengthRadios = ['6', '8', '10']
+        //       .map((value) => GestureDetector(
+        //             child: Row(
+        //               children: [
+        //                 Radio(
+        //                     value: value,
+        //                     groupValue: model.temporaryPasswordLength,
+        //                     onChanged: onChanged),
+        //                 Text(
+        //                   value,
+        //                   style: TextStyle(
+        //                       color: disabledTextColor(
+        //                           context, onChanged != null)),
+        //                 ),
+        //               ],
+        //             ).paddingOnly(right: 10),
+        //             onTap: () => onChanged?.call(value),
+        //           ))
+        //       .toList();
 
-          final isOptFixedNumOTP =
-              isOptionFixed(kOptionAllowNumericOneTimePassword);
-          final isNumOPTChangable = !isOptFixedNumOTP && tmpEnabled && !locked;
-          final numericOneTimePassword = GestureDetector(
-            child: InkWell(
-                child: Row(
-              children: [
-                Checkbox(
-                  enabled：false,
-                        value: model.allowNumericOneTimePassword,
-                        onChanged: isNumOPTChangable
-                            ? (bool? v) {
-                                model.switchAllowNumericOneTimePassword();
-                              }
-                            : null)
-                    .marginOnly(right: 5),
-                Expanded(
-                    child: Text(
-                  translate('Numeric one-time password'),
-                  style: TextStyle(
-                      color: disabledTextColor(context, isNumOPTChangable)),
-                ))
-              ],
-            )),
-            onTap: isNumOPTChangable
-                ? () => model.switchAllowNumericOneTimePassword()
-                : null,
-          ).marginOnly(left: _kContentHSubMargin - 5);
+        //   final isOptFixedNumOTP =
+        //       isOptionFixed(kOptionAllowNumericOneTimePassword);
+        //   final isNumOPTChangable = !isOptFixedNumOTP && tmpEnabled && !locked;
+        //   final numericOneTimePassword = GestureDetector(
+        //     child: InkWell(
+        //         child: Row(
+        //       children: [
+        //         Checkbox(
+        //           enabled：false,
+        //                 value: model.allowNumericOneTimePassword,
+        //                 onChanged: isNumOPTChangable
+        //                     ? (bool? v) {
+        //                         model.switchAllowNumericOneTimePassword();
+        //                       }
+        //                     : null)
+        //             .marginOnly(right: 5),
+        //         Expanded(
+        //             child: Text(
+        //           translate('Numeric one-time password'),
+        //           style: TextStyle(
+        //               color: disabledTextColor(context, isNumOPTChangable)),
+        //         ))
+        //       ],
+        //     )),
+        //     onTap: isNumOPTChangable
+        //         ? () => model.switchAllowNumericOneTimePassword()
+        //         : null,
+        //   ).marginOnly(left: _kContentHSubMargin - 5);
 
-          final modeKeys = <String>[
-            'password',
-            'click',
-            defaultOptionApproveMode
-          ];
-          final modeValues = [
-            translate('Accept sessions via password'),
-            translate('Accept sessions via click'),
-            translate('Accept sessions via both'),
-          ];
-          var modeInitialKey = model.approveMode;
-          if (!modeKeys.contains(modeInitialKey)) {
-            modeInitialKey = defaultOptionApproveMode;
-          }
-          final usePassword = model.approveMode != 'click';
+        //   final modeKeys = <String>[
+        //     'password',
+        //     'click',
+        //     defaultOptionApproveMode
+        //   ];
+        //   final modeValues = [
+        //     translate('Accept sessions via password'),
+        //     translate('Accept sessions via click'),
+        //     translate('Accept sessions via both'),
+        //   ];
+        //   var modeInitialKey = model.approveMode;
+        //   if (!modeKeys.contains(modeInitialKey)) {
+        //     modeInitialKey = defaultOptionApproveMode;
+        //   }
+        //   final usePassword = model.approveMode != 'click';
 
-          final isApproveModeFixed = isOptionFixed(kOptionApproveMode);
-          return _Card(title: 'Password',enabled：false, children: [
-            ComboBox(
-              enabled: false,
-              keys: modeKeys,
-              values: modeValues,
-              initialKey: modeInitialKey,
-              onChanged: (key) => model.setApproveMode(key),
-            ).marginOnly(left: _kContentHMargin),
-            if (usePassword) radios[0],
-            if (usePassword)
-              _SubLabeledWidget(
-                  context,
-                  'One-time password length',
-                  Row(
-                    children: [
-                      ...lengthRadios,
-                    ],
-                  ),
-                  enabled: tmpEnabled && !locked),
-            if (usePassword) numericOneTimePassword,
-            if (usePassword) radios[1],
-            if (usePassword && !isChangePermanentPasswordDisabled())
-              _SubButton('Set permanent password', setPasswordDialog,
-                  permEnabled && !locked),
-            // if (usePassword)
-            //   hide_cm(!locked).marginOnly(left: _kContentHSubMargin - 6),
-            if (usePassword) radios[2],
-          ]);
-        })));
+        //   final isApproveModeFixed = isOptionFixed(kOptionApproveMode);
+        //   return _Card(title: 'Password',enabled：false, children: [
+        //     ComboBox(
+        //       enabled: false,
+        //       keys: modeKeys,
+        //       values: modeValues,
+        //       initialKey: modeInitialKey,
+        //       onChanged: (key) => model.setApproveMode(key),
+        //     ).marginOnly(left: _kContentHMargin),
+        //     if (usePassword) radios[0],
+        //     if (usePassword)
+        //       _SubLabeledWidget(
+        //           context,
+        //           'One-time password length',
+        //           Row(
+        //             children: [
+        //               ...lengthRadios,
+        //             ],
+        //           ),
+        //           enabled: tmpEnabled && !locked),
+        //     if (usePassword) numericOneTimePassword,
+        //     if (usePassword) radios[1],
+        //     if (usePassword && !isChangePermanentPasswordDisabled())
+        //       _SubButton('Set permanent password', setPasswordDialog,
+        //           permEnabled && !locked),
+        //     // if (usePassword)
+        //     //   hide_cm(!locked).marginOnly(left: _kContentHSubMargin - 6),
+        //     if (usePassword) radios[2],
+        //   ]);
+        // })));
   }
 
   Widget more(BuildContext context) {
